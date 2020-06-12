473706336880d514b9baf577c1e714f64096c18a
Alex <blumius@gmail.com>
2020-06-12 18:05:39 +0300

    Initial commit

---
+++ keyboard.go
@@ -0,0 +1,25 @@
+package keyboard
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// GetFloat func convert string to float64
+func GetFloat() (float64, error) {
+
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
+}
